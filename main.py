35447f5fa0f211ca3954f04cc5431be5e58eea5c
rahimyavuzturan <turanrahimyavuz@gmail.com>
2025-02-20 14:13:11 +0300

    init ai

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 import pygame
 from pong import Game
 
-width, height = 700, 500
-window = pygame.display.set_mode((width, height))
 
-game = Game(window, width, height)
 
-run = True
-clock = pygame.time.Clock()
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run
-            break
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
 
-    game.loop()
-    game.draw()
-    pygame.display.update()
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
 
-pygame.quit()
+
+    def test_ai(self):
+    
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run
+                    break
+
+            # tuşlarla çalışması için
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_w]:
+                game.move_paddle(left=True, up=True)
+            if keys[pygame.K_s]:
+                game.move_paddle(left=True, up=False)
+
+
+            game.loop()
+            game.draw(False, True) # kaç hit olduğunu göstercek
+            pygame.display.update()
+
+        pygame.quit()
+
